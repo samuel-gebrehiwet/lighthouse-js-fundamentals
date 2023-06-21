b4f7c1a0e89418dd734a89888072f61368065eab
samuel gerehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-06-21 02:01:00 +0000

    use conditional tell people who is eligibile to vote

--- conditionals.js
+++ conditionals.js
@@ -32,4 +32,8 @@ if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
 
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
 
