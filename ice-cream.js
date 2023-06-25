4e397674abbc50bf8c1eeea353116d66885c63c3
samuel gerehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-06-25 17:35:26 +0000

    Add ingrediets.js

---
+++ ice-cream.js
@@ -0,0 +1,6 @@
+const newFlavours = [...iceCreamFlavours, "root beer"];
+console.log(newFlavours);
+console.log(newFlavours[0]);
+console.log(newFlavours[newFlavours.length - 1]);
+console.log(newFlavours.length);
+
